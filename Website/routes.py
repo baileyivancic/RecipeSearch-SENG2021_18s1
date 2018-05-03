17761b97a373430857b1ecb61a2812a84d3b3842
Nabil Shaikh <Nabil.Shaikh.Nabil@gmail.com>
2018-05-03 13:28:41 +1000

    all links fixed

--- Website/routes.py
+++ Website/routes.py
@@ -16,3 +16,14 @@ def home():
 def results():
 	return render_template("results.html")
 
+@app.route("/login")
+def login():
+	return render_template("login.html")
+
+@app.route("/register")
+def register():
+	return render_template("register.html")
+
+@app.route("/savedresults")
+def savedresults():
+	return render_template("savedresults.html")
